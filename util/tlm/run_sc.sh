964648f25ecca581ff8aae25d1dca42446cceb41
chisuhua <chisuhua@sensetime.com>
2020-03-26 17:39:57 +0800

    gpgpu: adding tlm_slave_memtest
     add tlm test case
    
    Change-Id: I78c316bb94354e7cd2cb88730788f34f30989d7b

--- util/tlm/run_sc.sh
+++ util/tlm/run_sc.sh
@@ -5,7 +5,13 @@
 # run ../../run_sc.sh first
 # make sure use correct master/slave
 
-OPTION="-v -d CxxConfig"
+#OPTION="-v -d CxxConfig -d TrafficGen -d Event -e 10000"
+#OPTION="-v -d TrafficGen -d Event -e 2000000"
+#OPTION="-v -e 500000 -d TrafficGen -d Event"
+#OPTION="-v -e 500000 -d Event"
+#OPTION="-v -e 5000000 -d TrafficGen"
+#OPTION="-v -d TrafficGen"
+OPTION="-v -d TrafficGen -d MemTest"
 
 CMD="./build/examples/slave_port/gem5.sc m5out/config.ini $OPTION "
 #CMD="./build/examples/master_port/gem5.sc m5out/config.ini $OPTION "
