31c0290b66012c7c53cb8d6872850dc9711ba812
Chi Suhua <chisuhua@gmail.com>
2022-02-27 23:29:28 +0800

    update opu elfio
    Change-Id: I336e5f016dc916e045912253ffee1bc9db6c2311

--- design/opu/git_clone.sh
+++ design/opu/git_clone.sh
@@ -4,3 +4,4 @@ git clone git@github.com:chisuhua/coasm.git
 git clone git@github.com:chisuhua/isasim.git
 git clone git@github.com:chisuhua/umd.git
 git clone https://gitee.com/chisuhua/py3antlr4book.git
+git clone https://gitee.com/mirrors/elfio.git 3rdparty/elfio
